f9329cae766c21d0d61a769c8ade2623e26b658a
杨旗 <yangq06@mingyuanyun.com>
2018-10-29 21:08:22 +0800

    add fetch_blocks.json

--- data/fetch_blocks.js
+++ data/fetch_blocks.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const path = require('path')
-const JSONtoCSV = require('../utils').JSONtoCSV
+const JSONtoCSV = require('../util').JSONtoCSV
+var blockexplorer = require('blockchain.info/blockexplorer')
 
 const fileName = 'blocks.csv'
 const filePath = path.join(__dirname, fileName)
-
+const columns = ['height', 'time', 'hash', 'main_chain']
 
 function remainDays(lastTime) {
   const milSec = new Date() - new Date(lastTime)
@@ -21,11 +22,14 @@ function sleep(sec) {
 function saveToFile(list) {
   const d0 = new Date()
   console.log('start save to file')
-  const dataToCsv = JSONtoCSV(list, CONFIG.columns)
+  const dataToCsv = JSONtoCSV(list, columns)
   fs.appendFileSync(fileName, dataToCsv + '\n')
   const sec = (new Date() - d0) / 1000
   console.log('end save to file, second:', sec)
 }
+
+let lastTimestamp = 1231006505000
+let lastHeight = -1
 
 // init request start time
 if (fs.existsSync(filePath)) {
@@ -35,9 +39,50 @@ if (fs.existsSync(filePath)) {
   if (len > 0) {
     const last = csvList[len - 2]
     if (last) {
-      const timestamp = last.split(',')[0]
-      lastTimestamp = timestamp
-      console.log('lastTimestamp => ', timestamp)
+      const splits = last.split(',')
+      const timestamp = splits[1]
+      lastHeight = splits[0]
+      lastTimestamp = timestamp * 1000
+      console.log('height => ', lastHeight, 'lastTimestamp => ', lastTimestamp)
     }
   }
 }
+
+async function run() {
+  let hasNewData = new Date(lastTimestamp) < new Date()
+  if (!hasNewData) {
+    console.log('has no new data, exit')
+  }
+  while(hasNewData) {
+    const remains = Math.round(remainDays(lastTimestamp))
+    console.log('remain days:', remains)
+    if (remains === 0) {
+      console.log('remain is 0, finished')
+      break
+    }
+    lastTimestamp = lastTimestamp + 24 * 3600 * 1000// 往后24小时
+    let requestTime = lastTimestamp
+    try {
+      const res = await blockexplorer.getBlocks(requestTime)
+      const list = res.blocks
+      const len = list.length
+      if (len === 0) {
+        console.log('data len is 0')
+      } else {
+        newBlockHeight = list[0].height
+        if (newBlockHeight - lastHeight !== 1) {
+          console.log('height wrong')
+          process.exit(1)
+        }
+        lastHeight = list[list.length - 1].height
+        saveToFile(list)
+      }
+
+      sleep(1)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+
+run()
