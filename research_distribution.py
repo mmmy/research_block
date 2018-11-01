b8108b3a844a82b2eea298a48e96911478a20f97
杨旗 <yangq06@mingyuanyun.com>
2018-11-01 19:14:19 +0800

    update

--- research_distribution.py
+++ research_distribution.py
@@ -2,18 +2,27 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import hashlib
 
 file = './data/blocks.csv'
 
 blocks = pd.read_csv(file, header=None, names=['height', 'time', 'hash', 'main_chain'])
 
-index = -2
+hash_list = blocks.hash
+hash_list = [hashlib.sha256(h.encode('utf-8')).hexdigest() for h in hash_list]
 
-codes = [h[index] for h in blocks.hash]
+codes = [list(h) for h in hash_list]
 
-codes_hist = pd.Series(codes).value_counts()
+codes_pd = pd.DataFrame(codes)
 
-codes_hist = codes_hist.sort_index(axis=0)
+codes_hist = [pd.Series(codes_pd[c]).value_counts().sort_index(axis=0) for c in codes_pd.columns]
 
-plt.bar(codes_hist.index, codes_hist.values)
+fig, axes = plt.subplots(4, 16)
+
+axes = axes.ravel()
+for i in range(len(axes)):
+  hist = codes_hist[i]
+  ax = axes[i]
+  ax.bar(hist.index, hist.values)
+  ax.set_title(i)
 plt.show()
