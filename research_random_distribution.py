f5f788e5b94201750e26e3c444e2fc3d3faa4c84
yangqi <531905408@qq.com>
2018-11-07 00:55:25 +0800

    update

--- research_random_distribution.py
+++ research_random_distribution.py
@@ -14,7 +14,7 @@ def sample():
 sample_hist = [pd.Series(sample()).value_counts().sort_index(axis=0) for i in range(64)]
 codes_hist_pd = pd.DataFrame(sample_hist)
 codes_hist_sum = codes_hist_pd.apply(np.sum)
-print(codes_hist_sum.apply(lambda x:stats.chi2_contingency([x, [2189952]*16]), axis=1))
+print(stats.chi2_contingency([codes_hist_sum.values, [2189952]*16]))
 
 result = codes_hist_pd.apply(lambda x:stats.chi2_contingency([x, [34281]*16]), axis=1)
 print(result)
