08c19ec7b7a04fc638f081c401be2102e2e63783
杨旗 <yangq06@mysoft.com.cn>
2019-01-19 16:37:25 +0800

    u

--- app/benchmark_realdata/task.sh
+++ app/benchmark_realdata/task.sh
@@ -2,6 +2,7 @@ base_dir="/root/workspace/research_block/app/benchmark_realdata"
 mkdir "${base_dir}/temp"
 # 爬虫同步最新ssq求数据
 python3 "${base_dir}/main.py"
+# 区块链生成数据
+node "${base_dir}/../server/sync_benchmark.js"
 # 生成图表和csv
 python3 "${base_dir}/statistic_ssq.py"
-
