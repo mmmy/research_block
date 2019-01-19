69965e5283dfd5ce4abd274ffed18d5635538fc3
杨旗 <yangq06@mysoft.com.cn>
2019-01-19 15:20:42 +0800

    u

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,3 +2,7 @@ systemctl enable blockapp-server.service
 systemctl enable btc-block-sync.service
 systemctl enable nginx.service
 systemctl daemon-reload
+
+systemctl start blockapp-server.service
+systemctl start btc-block-sync.service
+systemctl start nginx.service
